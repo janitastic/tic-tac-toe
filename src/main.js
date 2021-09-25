b89790c2fab50d1515430a621fe26980fcd53c13
Jani Palomino <palomino.jani@gmail.com>
2021-09-25 16:28:10 -0600

    Pseudocode querySelectors & eventListeners

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-var game = new Game();
+var currentGame = new Game();
+
+// QUERY SELECTORS
+// Will need the following
+//   - "all" game board buttons
+//   - player turn title
+//   - player turn image/icon/token
+//   - winner display title
+//     - maybe winner image/icon/token, if can't use player turn img
+//   - "it's a draw" display title
+//   - Player 1 win count
+//   - Player 2 win count
+//   - Start New Game button
+
+//EVENT LISTENERS
+// - Window onload
+// - click on gameboard
+// - click on new game button
