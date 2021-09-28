7953280f69d8e4fd01bdf644cfc9676af1b9c6ef
Jani Palomino <palomino.jani@gmail.com>
2021-09-27 19:40:16 -0600

    Update game class

--- src/game.js
+++ src/game.js
@@ -1,9 +1,32 @@
 class Game {
   constructor() {
-    this.player1 = new Player();
-    this.player2 = new Player();
+    this.player1 = new Player('player1', 'dog');
+    this.player2 = new Player('player2', 'cat');
+    this.isWinner = false;
+    this.wins = [];
   }
-}
+
+  checkWinningPlays(currentPlayer) {
+    var winningPlays = [
+      [1, 4, 7],//0
+      [2, 5, 8],//1
+      [3, 6, 9],//2
+      [1, 2, 3],//3
+      [4, 5, 6],//4
+      [7, 8, 9],//5
+      [1, 5, 9],//6
+      [3, 5, 7]//7
+    ];
+    for (var i = 0; i < winningPlays.length; i++) {
+      if (currentPlayer.plays.includes(winningPlays[i][0]) && currentPlayer.plays.includes(winningPlays[i][1]) && currentPlayer.plays.includes(winningPlays[i][2])) {
+        currentPlayer.isWinner = true;
+        currentPlayer.wins++;
+        this.isWinner = true;
+      }
+    }
+  }
+};
+
 
 /*
 Additionally file should include -
